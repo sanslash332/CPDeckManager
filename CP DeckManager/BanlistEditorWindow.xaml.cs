123616dde04210c2d1aa2cee2a0df28e80820280
sanslash332 <sanslash332@yahoo.es>
2017-11-11 01:41:46 -0300

    me falto agregar lo nuevo sobre la banlist window

---
+++ CP DeckManager/BanlistEditorWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CP_DeckManager
+{
+    /// <summary>
+    /// Interaction logic for BanlistEditorWindow.xaml
+    /// </summary>
+    public partial class BanlistEditorWindow : Window
+    {
+        public BanlistEditorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+
+        }
+    }
+}
